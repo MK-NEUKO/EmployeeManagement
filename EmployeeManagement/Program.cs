5c6215b8947f61adb44e5cf70db31d57b48f5469
Michael Koch <m.koch@neuko-devment.com>
2024-12-18 22:36:03 +0100

    Add project files.

---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,35 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(options => 
+                options.UseInMemoryDatabase("EmployeeDb")
+                );
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
+            var app = builder.Build();
+
+            app.UseCors("MyCors");
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
